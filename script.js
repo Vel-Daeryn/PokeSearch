41e62418a9df54901e36a8d57b648000f9d3dce5
Taverniers Dylan <252625083+Vel-Daeryn@users.noreply.github.com>
2026-07-05 02:50:14 +0200

    [MAJ] 1ère génération ajoutée
    - Besoin de compléter les TAGS de types
    - Fournir des traduction pour les types
    - Faire en sorte que l'ordre corresponde à l'ID du pokémon
    - Ajouter un ALT="" aux img
    - Ne pas charger l'intégralité du "pokédex" d'un coup
    - Faire des cards des cards clickables
    - Page "individuelle" à faire
    - Recherche manuelle à ajouter
    - Recherche par TAG à ajouter.

--- script.js
+++ script.js
@@ -19,48 +19,88 @@ searchBar.addEventListener("click", (e) => {
 
 /* fetch API pokémon*/
 
-fetch("https://pokeapi.co/api/v2/pokemon-species/58")
-    .then(response => response.json())
-    .then(data => {
-        /*console.log(data.sprites.other["official-artwork"].front_default) /*sprite de face*/
-        
-        const frenchName = data.names[4].name
-        const pokemonId = data.id
-        
-        fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonId)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
+const pokemonList = []
+console.log(pokemonList);
 
-                const pokemonSprite = data.sprites.other["official-artwork"].front_default
 
-                
-                
-            })
-            .catch(error => console.error(error))
-    })
-    .catch(error => console.error(error))
+for(let i = 1; i <= 151; i++) {
+    fetch("https://pokeapi.co/api/v2/pokemon-species/" + i)
+        .then(response => response.json())
+        .then(data => {
+            /*console.log(data.sprites.other["official-artwork"].front_default) /*sprite de face*/
+            
+            const frenchName = data.names[4].name
+            const pokemonId = data.id
+    
+            pokemonList.push(frenchName)
+            
+            fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonId)
+                .then(response => response.json())
+                .then(data => {
+    
+                    pagination(data)
+                    
+                    
+                })
+                .catch(error => console.error(error))
+        })
+        .catch(error => console.error(error))
+}
 
 /* Pagination */
 
 
 const pagination = (data) => {
-    /*<div class="poke-cards">
+    /*
+    Pagination in HTML -> 
+    -----------------
+    <div class="poke-cards">
     <h2>Caninos</h2>
     <img src="res/img/250px-template.png" alt="Une image de Caninos">
-    <p class="id"><span>n°</span>0058</p>
+    <p class="id">n°58</p>
     <span class="type-tag fire">Feu</span>
-    </div>*/
+    </div>
+    */
     const start = document.querySelector("main")
     const createDiv = document.createElement("div")
 
     start.appendChild(createDiv).classList.add("poke-cards")
 
-    const pokemonName = document.createElement("h2").innerText = "Caninos"
+    const pokemonName = document.createElement("h2")
 
+    createDiv.appendChild(pokemonName)
+    pokemonName.innerText = `${pokemonList[data.id]}`
+
+    const pokemonImg = document.createElement("img")
+
+    createDiv.appendChild(pokemonImg).src = `${data.sprites.other["official-artwork"].front_default}`
+
+    const pokemonNumber = document.createElement("p")
+
+    createDiv.appendChild(pokemonNumber).classList.add("id")
+    pokemonNumber.innerText = `n°${data.id}`
+
+    const typeTag = document.createElement("span")
+    const typeTagSecond = document.createElement("span")
     
-}
+    const pokemonType = []
+    data.types.forEach(element => {
+        pokemonType.push(element.type.name) 
+    })
 
-pagination()
+
+    if(pokemonType.length === 1) {
+        createDiv.appendChild(typeTag).setAttribute("class", `type-tag ${pokemonType[0]}`)
+        typeTag.innerText = `${pokemonType[0]}`
+    } else {
+        createDiv.appendChild(typeTag).setAttribute("class", `type-tag ${pokemonType[0]}`)
+        typeTag.innerText = `${pokemonType[0]}`
+
+        createDiv.appendChild(typeTagSecond).setAttribute("class", `type-tag ${pokemonType[1]}`)
+        typeTagSecond.innerText = `${pokemonType[1]}`
+    }
+
+ 
+}
 
 
